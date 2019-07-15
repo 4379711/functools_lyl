d00ce2a936db14422aae2ec46c74c64e5cac5a20
liuyalong <4379711@qq.com>
2019-07-15 15:34:32 +0800

    ...

--- WhatTheFuck/__init__.py
+++ WhatTheFuck/__init__.py
@@ -6,15 +6,15 @@
 说明：
     1.schedule_lyl更改自schedule，修复日期不准确BUG，并添加线程控制以解决任务延迟的问题
     使用方法：
-        from functools_lyl import schedule_lyl
+        from WhatTheFuck import schedule_lyl
 
     2.mylog:日志记录,自动切割，压缩等
-    使用方法: from functools_lyl.mylog import My_log
+    使用方法: from WhatTheFuck.mylog import My_log
               logger=My_log('dirpath').getlogger()
 
     3.timeslimit :控制函数执行频率
     使用方法：
-        from functools_lyl.timeslimit import CallTimesLimit
+        from WhatTheFuck.timeslimit import CallTimesLimit
 
         每4秒执行5次abc
 
--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ with open("README.md", "r", encoding='utf-8') as fh:
     long_description = fh.read()
 
 setup(
-    name='functools_lyl',  # '库的名称,一般写成文件夹的名字就可以了
+    name='WhatTheFuck',  # '库的名称,一般写成文件夹的名字就可以了
     version="0.1.1",  # 版本，每次发版本都不能重复，每次发版必须改这个地方
     description=(
-        'Some very useful tools'  # 一个简介，别人搜索包时候，这个概要信息会显示在在搜索列表中
+        'What The Fuck !'  # 一个简介，别人搜索包时候，这个概要信息会显示在在搜索列表中
     ),
     long_description=long_description,  # 这是详细的，一般是交别人怎么用，很多包没写，那么在官网就造成没有使用介绍了
     long_description_content_type="text/markdown",
@@ -29,6 +29,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Software Development :: Libraries'
     ],
     install_requires=[  # 这里是依赖列表，表示运行这个包的运行某些功能还需要你安装其他的包
